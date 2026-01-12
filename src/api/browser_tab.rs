d32661e32032e5ca53ddfe8b6aa7ff1e34cf67a1
Wilson Lin <code@wilsonl.in>
2026-01-12 09:14:44 -0800

    fix: remove duplicate MouseEvent.detail field
    Fix E0062 in BrowserTab::dispatch_click_event_with_pointer by removing the duplicated MouseEvent.detail initializer.

--- src/api/browser_tab.rs
+++ src/api/browser_tab.rs
@@ -5282,7 +5282,6 @@ impl BrowserTab {
       detail: 1,
       button: dom_button,
       buttons: dom_buttons,
-      detail: 1,
       ctrl_key: modifiers.ctrl(),
       shift_key: modifiers.shift(),
       alt_key: modifiers.alt(),
