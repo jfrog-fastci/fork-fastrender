1256495c4f669f314a7e22513f8dee385026482f
Wilson Lin <code@wilsonl.in>
2026-01-12 06:33:56 -0800

    chore(vm-js): keep RegExpProgram::try_clone fallible

--- vendor/ecma-rs/vm-js/src/regexp.rs
+++ vendor/ecma-rs/vm-js/src/regexp.rs
@@ -589,8 +589,9 @@ impl RegExpProgram {
         },
         Inst::Match => Inst::Match,
       };
-      // `insts` was reserved to `self.insts.len()` above; pushing within that bound is infallible.
-      insts.push(cloned);
+      // `insts` was reserved to `self.insts.len()` above; pushing within that bound should not need
+      // to grow the Vec, but keep the push fallible to uphold the `try_clone` contract.
+      vec_try_push_vm(&mut insts, cloned)?;
     }
 
     Ok(Self {
