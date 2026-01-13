103a883f88e46c205e1b1ac9edbdba0db16dcaa5
Wilson Lin <code@wilsonl.in>
2026-01-13 14:28:23 -0800

    test(vm-js): split generator yield operator baseline coverage
    Refactor vendor/ecma-rs/vm-js/tests/generators_yield_operators.rs into
    per-operator tests so each yield-in-expression form is isolated and can fail
    independently (binary ops, assignments, updates, new/delete).

--- vendor/ecma-rs/vm-js/tests/generators_yield_operators.rs
+++ vendor/ecma-rs/vm-js/tests/generators_yield_operators.rs
@@ -7,52 +7,17 @@ fn new_runtime() -> JsRuntime {
 }
 
 #[test]
-fn generators_yield_in_binary_expressions() {
+fn generators_yield_in_binary_addition_left() {
   let mut rt = new_runtime();
   let value = rt
     .exec_script(
       r#"
-        // (yield 1) + 2
-        function* add_left() { return (yield 1) + 2; }
-        const it1 = add_left();
-        const a1 = it1.next();
-        const a2 = it1.next(10);
-        const ok1 = a1.value === 1 && a1.done === false && a2.value === 12 && a2.done === true;
-
-        // 1 + (yield 2)
-        function* add_right() { return 1 + (yield 2); }
-        const it2 = add_right();
-        const b1 = it2.next();
-        const b2 = it2.next(10);
-        const ok2 = b1.value === 2 && b1.done === false && b2.value === 11 && b2.done === true;
-
-        // (yield 1) * 2
-        function* mul_left() { return (yield 1) * 2; }
-        const it3 = mul_left();
-        const c1 = it3.next();
-        const c2 = it3.next(6);
-        const ok3 = c1.value === 1 && c1.done === false && c2.value === 12 && c2.done === true;
-
-        // (yield 2) ** 3
-        function* exp_left() { return (yield 2) ** 3; }
-        const it4 = exp_left();
-        const d1 = it4.next();
-        const d2 = it4.next(4);
-        const ok4 = d1.value === 2 && d1.done === false && d2.value === 64 && d2.done === true;
-
-        // (yield 1) === (yield 1)
-        function* strict_eq_both() { return (yield 1) === (yield 1); }
-        const it5 = strict_eq_both();
-        const e1 = it5.next();
-        // Ensure each yield consumes its own resume value.
-        const e2 = it5.next(7);
-        const e3 = it5.next(8);
-        const ok5 =
-          e1.value === 1 && e1.done === false &&
-          e2.value === 1 && e2.done === false &&
-          e3.value === false && e3.done === true;
-
-        ok1 && ok2 && ok3 && ok4 && ok5
+        function* g() { return (yield 1) + 2; }
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next(10);
+        r1.value === 1 && r1.done === false &&
+        r2.value === 12 && r2.done === true
       "#,
     )
     .unwrap();
@@ -60,67 +25,184 @@ fn generators_yield_in_binary_expressions() {
 }
 
 #[test]
-fn generators_yield_in_assignment_expressions() {
+fn generators_yield_in_binary_addition_right() {
   let mut rt = new_runtime();
   let value = rt
     .exec_script(
       r#"
-        // x = (yield 1)
-        function* assign_rhs() {
+        function* g() { return 1 + (yield 2); }
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next(10);
+        r1.value === 2 && r1.done === false &&
+        r2.value === 11 && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generators_yield_in_binary_multiplication_left() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() { return (yield 1) * 2; }
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next(6);
+        r1.value === 1 && r1.done === false &&
+        r2.value === 12 && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generators_yield_in_binary_exponentiation_left() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() { return (yield 2) ** 3; }
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next(4);
+        r1.value === 2 && r1.done === false &&
+        r2.value === 64 && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generators_yield_in_binary_strict_equality_both() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() { return (yield 1) === (yield 1); }
+        const it = g();
+        const r1 = it.next();
+        // Ensure each yield consumes its own resume value.
+        const r2 = it.next(7);
+        const r3 = it.next(8);
+        r1.value === 1 && r1.done === false &&
+        r2.value === 1 && r2.done === false &&
+        r3.value === false && r3.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generators_yield_in_assignment_rhs() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
           let x = 0;
           const r = (x = (yield 1));
           return r === 10 && x === 10;
         }
-        const it1 = assign_rhs();
-        const a1 = it1.next();
-        const a2 = it1.next(10);
-        const ok1 = a1.value === 1 && a1.done === false && a2.value === true && a2.done === true;
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next(10);
+        r1.value === 1 && r1.done === false &&
+        r2.value === true && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
 
-        // x += (yield 1)
-        function* add_assign_rhs() {
+#[test]
+fn generators_yield_in_assignment_addition_rhs() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
           let x = 5;
           const r = (x += (yield 1));
           return r === 12 && x === 12;
         }
-        const it2 = add_assign_rhs();
-        const b1 = it2.next();
-        const b2 = it2.next(7);
-        const ok2 = b1.value === 1 && b1.done === false && b2.value === true && b2.done === true;
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next(7);
+        r1.value === 1 && r1.done === false &&
+        r2.value === true && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
 
-        // x **= (yield 1)
-        function* exp_assign_rhs() {
+#[test]
+fn generators_yield_in_assignment_exponentiation_rhs() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
           let x = 2;
           const r = (x **= (yield 1));
           return r === 8 && x === 8;
         }
-        const it3 = exp_assign_rhs();
-        const c1 = it3.next();
-        const c2 = it3.next(3);
-        const ok3 = c1.value === 1 && c1.done === false && c2.value === true && c2.done === true;
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next(3);
+        r1.value === 1 && r1.done === false &&
+        r2.value === true && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
 
-        // o[(yield "a")] = 1
-        function* member_assign_key() {
+#[test]
+fn generators_yield_in_member_assignment_computed_key() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
           const o = {};
-          o[(yield "a")] = 1;
-          return o.b === 1 && o.a === undefined;
+          const r = (o[(yield "a")] = 1);
+          return r === 1 && o.b === 1 && o.a === undefined;
         }
-        const it4 = member_assign_key();
-        const d1 = it4.next();
-        const d2 = it4.next("b");
-        const ok4 = d1.value === "a" && d1.done === false && d2.value === true && d2.done === true;
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next("b");
+        r1.value === "a" && r1.done === false &&
+        r2.value === true && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
 
-        // o[(yield "a")] += 2
-        function* member_add_assign_key() {
+#[test]
+fn generators_yield_in_member_addition_assignment_computed_key() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
           const o = { b: 1 };
           const r = (o[(yield "a")] += 2);
           return r === 3 && o.b === 3 && o.a === undefined;
         }
-        const it5 = member_add_assign_key();
-        const e1 = it5.next();
-        const e2 = it5.next("b");
-        const ok5 = e1.value === "a" && e1.done === false && e2.value === true && e2.done === true;
-
-        ok1 && ok2 && ok3 && ok4 && ok5
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next("b");
+        r1.value === "a" && r1.done === false &&
+        r2.value === true && r2.done === true
       "#,
     )
     .unwrap();
@@ -128,34 +210,21 @@ fn generators_yield_in_assignment_expressions() {
 }
 
 #[test]
-fn generators_yield_in_update_expressions() {
+fn generators_yield_in_member_postfix_update_computed_key() {
   let mut rt = new_runtime();
   let value = rt
     .exec_script(
       r#"
-        // o[(yield "a")]++
-        function* member_postfix_update_key() {
+        function* g() {
           const o = { b: 1 };
           const r = o[(yield "a")]++;
           return r === 1 && o.b === 2 && o.a === undefined;
         }
-        const it1 = member_postfix_update_key();
-        const a1 = it1.next();
-        const a2 = it1.next("b");
-        const ok1 = a1.value === "a" && a1.done === false && a2.value === true && a2.done === true;
-
-        // ++o[(yield "a")]
-        function* member_prefix_update_key() {
-          const o = { b: 1 };
-          const r = ++o[(yield "a")];
-          return r === 2 && o.b === 2 && o.a === undefined;
-        }
-        const it2 = member_prefix_update_key();
-        const b1 = it2.next();
-        const b2 = it2.next("b");
-        const ok2 = b1.value === "a" && b1.done === false && b2.value === true && b2.done === true;
-
-        ok1 && ok2
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next("b");
+        r1.value === "a" && r1.done === false &&
+        r2.value === true && r2.done === true
       "#,
     )
     .unwrap();
@@ -163,34 +232,65 @@ fn generators_yield_in_update_expressions() {
 }
 
 #[test]
-fn generators_yield_in_new_and_delete_expressions() {
+fn generators_yield_in_member_prefix_update_computed_key() {
   let mut rt = new_runtime();
   let value = rt
     .exec_script(
       r#"
-        // new C(yield 1)
+        function* g() {
+          const o = { b: 1 };
+          const r = ++o[(yield "a")];
+          return r === 2 && o.b === 2 && o.a === undefined;
+        }
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next("b");
+        r1.value === "a" && r1.done === false &&
+        r2.value === true && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generators_yield_in_new_arg() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
         function C(x) { this.x = x; }
-        function* ctor_arg() {
+        function* g() {
           const o = new C(yield 1);
           return o.x === 42;
         }
-        const it1 = ctor_arg();
-        const a1 = it1.next();
-        const a2 = it1.next(42);
-        const ok1 = a1.value === 1 && a1.done === false && a2.value === true && a2.done === true;
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next(42);
+        r1.value === 1 && r1.done === false &&
+        r2.value === true && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
 
-        // delete o[(yield "a")]
-        function* delete_member_key() {
+#[test]
+fn generators_yield_in_delete_computed_key() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
           const o = { b: 1 };
           const r = delete o[(yield "a")];
           return r === true && !("b" in o) && !("a" in o);
         }
-        const it2 = delete_member_key();
-        const b1 = it2.next();
-        const b2 = it2.next("b");
-        const ok2 = b1.value === "a" && b1.done === false && b2.value === true && b2.done === true;
-
-        ok1 && ok2
+        const it = g();
+        const r1 = it.next();
+        const r2 = it.next("b");
+        r1.value === "a" && r1.done === false &&
+        r2.value === true && r2.done === true
       "#,
     )
     .unwrap();
