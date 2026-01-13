df57fa8dcc466ff2667bef59cd0c6fe7f52d45db
Wilson Lin <code@wilsonl.in>
2026-01-12 21:39:25 -0800

    fix(vm-js): import UnicodeStringProperty for non-test builds

--- vendor/ecma-rs/vm-js/src/regexp.rs
+++ vendor/ecma-rs/vm-js/src/regexp.rs
@@ -13,7 +13,9 @@
 //! Call sites must treat compilation failures as `SyntaxError`.
 
 use crate::fallible_alloc::box_try_new_vm;
-use crate::regexp_unicode_property_strings::{match_property_at, MAX_MATCHES_PER_POSITION};
+use crate::regexp_unicode_property_strings::{
+  match_property_at, UnicodeStringProperty, MAX_MATCHES_PER_POSITION,
+};
 use crate::regexp_unicode_resolver::{resolve_unicode_property_value_expression, ResolvedUnicodeProperty};
 use crate::regexp_unicode_tables::{
   contains_code_point as unicode_table_contains_code_point, BinaryProp, ResolvedCodePointProperty,
