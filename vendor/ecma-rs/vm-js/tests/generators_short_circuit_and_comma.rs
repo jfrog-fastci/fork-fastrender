bef3278427e5c9fbf193104d96fc2893a9f8ded3
Wilson Lin <code@wilsonl.in>
2026-01-13 14:59:34 -0800

    test(vm-js): add yield-in-LHS short-circuit generator cases
    - Added generator tests for &&/||/?? where the LHS is a yield expression, verifying RHS is evaluated (or skipped) based on the resumed LHS value.

--- vendor/ecma-rs/vm-js/tests/generators_short_circuit_and_comma.rs
+++ vendor/ecma-rs/vm-js/tests/generators_short_circuit_and_comma.rs
@@ -40,6 +40,44 @@ fn generator_and_evaluates_rhs_when_truthy_and_can_yield() {
 }
 
 #[test]
+fn generator_and_short_circuiting_after_yield_in_lhs_skips_rhs() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      function* g(){ return (yield false) && (yield 1); }
+      var it = g();
+      var r1 = it.next();
+      var r2 = it.next(0);
+      r1.done === false && r1.value === false &&
+      r2.done === true && r2.value === 0
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_and_rhs_is_evaluated_after_yield_in_lhs_when_truthy() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      function* g(){ return (yield true) && (yield 1); }
+      var it = g();
+      var r1 = it.next();
+      var r2 = it.next(1);
+      var r3 = it.next(42);
+      r1.done === false && r1.value === true &&
+      r2.done === false && r2.value === 1 &&
+      r3.done === true && r3.value === 42
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
 fn generator_or_short_circuit_prevents_yield() {
   let mut rt = new_runtime();
   let value = rt
@@ -73,6 +111,44 @@ fn generator_or_evaluates_rhs_when_falsy_and_can_yield() {
 }
 
 #[test]
+fn generator_or_short_circuiting_after_yield_in_lhs_skips_rhs() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      function* g(){ return (yield true) || (yield 1); }
+      var it = g();
+      var r1 = it.next();
+      var r2 = it.next(1);
+      r1.done === false && r1.value === true &&
+      r2.done === true && r2.value === 1
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_or_rhs_is_evaluated_after_yield_in_lhs_when_falsy() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      function* g(){ return (yield false) || (yield 1); }
+      var it = g();
+      var r1 = it.next();
+      var r2 = it.next(0);
+      var r3 = it.next(42);
+      r1.done === false && r1.value === false &&
+      r2.done === false && r2.value === 1 &&
+      r3.done === true && r3.value === 42
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
 fn generator_nullish_coalescing_short_circuit_prevents_yield_when_not_nullish() {
   let mut rt = new_runtime();
   let value = rt
@@ -99,6 +175,44 @@ fn generator_nullish_coalescing_evaluates_rhs_when_nullish_and_can_yield() {
       var r1 = it.next();
       var r2 = it.next(42);
       r1.done === false && r1.value === 1 && r2.done === true && r2.value === 42
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_nullish_coalescing_short_circuiting_after_yield_in_lhs_skips_rhs() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      function* g(){ return (yield null) ?? (yield 1); }
+      var it = g();
+      var r1 = it.next();
+      var r2 = it.next(0);
+      r1.done === false && r1.value === null &&
+      r2.done === true && r2.value === 0
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_nullish_coalescing_rhs_is_evaluated_after_yield_in_lhs_when_nullish() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      function* g(){ return (yield undefined) ?? (yield 1); }
+      var it = g();
+      var r1 = it.next();
+      var r2 = it.next(undefined);
+      var r3 = it.next(42);
+      r1.done === false && r1.value === undefined &&
+      r2.done === false && r2.value === 1 &&
+      r3.done === true && r3.value === 42
     "#,
     )
     .unwrap();
