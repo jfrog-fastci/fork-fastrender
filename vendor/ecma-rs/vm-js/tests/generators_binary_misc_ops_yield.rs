326cb36695086901b6f4c40efa7e323099261513
Wilson Lin <code@wilsonl.in>
2026-01-13 13:26:31 -0800

    test(vm-js): strengthen misc yield/binary operator coverage
    - Add both-operands yield coverage for shift operators (<<, >>, >>>)
    - Assert yield result objects' done/value ordering across misc operator tests
    - Bump heap size to 2MiB to reduce flakiness as builtin surface area grows

--- vendor/ecma-rs/vm-js/tests/generators_binary_misc_ops_yield.rs
+++ vendor/ecma-rs/vm-js/tests/generators_binary_misc_ops_yield.rs
@@ -2,7 +2,9 @@ use vm_js::{Heap, HeapLimits, JsRuntime, Value, Vm, VmOptions};
 
 fn new_runtime() -> JsRuntime {
   let vm = Vm::new(VmOptions::default());
-  let heap = Heap::new(HeapLimits::new(1024 * 1024, 1024 * 1024));
+  // Keep a bit of headroom over the minimal 1MiB heaps used by some unit tests so these generator
+  // tests don't become flaky as builtin surface area grows.
+  let heap = Heap::new(HeapLimits::new(2 * 1024 * 1024, 2 * 1024 * 1024));
   JsRuntime::new(vm, heap).unwrap()
 }
 
@@ -17,7 +19,9 @@ fn bitwise_or_with_yield_in_both_operands() {
         var r1 = it.next();
         var r2 = it.next(1);
         var r3 = it.next(2);
-        r1.value === 1 && r2.value === 2 && r3.done === true && r3.value === 3
+        r1.value === 1 && r1.done === false &&
+        r2.value === 2 && r2.done === false &&
+        r3.done === true && r3.value === 3
       "#,
     )
     .unwrap();
@@ -35,7 +39,9 @@ fn bitwise_and_with_yield_in_both_operands() {
         var r1 = it.next();
         var r2 = it.next(6);
         var r3 = it.next(3);
-        r1.value === 1 && r2.value === 2 && r3.done === true && r3.value === 2
+        r1.value === 1 && r1.done === false &&
+        r2.value === 2 && r2.done === false &&
+        r3.done === true && r3.value === 2
       "#,
     )
     .unwrap();
@@ -53,7 +59,9 @@ fn bitwise_xor_with_yield_in_both_operands() {
         var r1 = it.next();
         var r2 = it.next(5);
         var r3 = it.next(1);
-        r1.value === 1 && r2.value === 2 && r3.done === true && r3.value === 4
+        r1.value === 1 && r1.done === false &&
+        r2.value === 2 && r2.done === false &&
+        r3.done === true && r3.value === 4
       "#,
     )
     .unwrap();
@@ -68,9 +76,10 @@ fn left_shift_with_yield_in_rhs() {
       r#"
         function* g(){ return 1 << (yield 1); }
         var it = g();
-        it.next();
-        var r = it.next(2);
-        r.done === true && r.value === 4
+        var r1 = it.next();
+        var r2 = it.next(2);
+        r1.value === 1 && r1.done === false &&
+        r2.done === true && r2.value === 4
       "#,
     )
     .unwrap();
@@ -85,9 +94,10 @@ fn unsigned_right_shift_with_yield_in_rhs() {
       r#"
         function* g(){ return (-1) >>> (yield 0); }
         var it = g();
-        it.next();
-        var r = it.next(0);
-        r.done === true && r.value === 4294967295
+        var r1 = it.next();
+        var r2 = it.next(0);
+        r1.value === 0 && r1.done === false &&
+        r2.done === true && r2.value === 4294967295
       "#,
     )
     .unwrap();
@@ -102,9 +112,70 @@ fn signed_right_shift_with_yield_in_rhs() {
       r#"
         function* g(){ return 8 >> (yield 1); }
         var it = g();
-        it.next();
-        var r = it.next(2);
-        r.done === true && r.value === 2
+        var r1 = it.next();
+        var r2 = it.next(2);
+        r1.value === 1 && r1.done === false &&
+        r2.done === true && r2.value === 2
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn left_shift_with_yield_in_both_operands() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g(){ return (yield 1) << (yield 2); }
+        var it = g();
+        var r1 = it.next();
+        var r2 = it.next(1);
+        var r3 = it.next(2);
+        r1.value === 1 && r1.done === false &&
+        r2.value === 2 && r2.done === false &&
+        r3.done === true && r3.value === 4
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn signed_right_shift_with_yield_in_both_operands() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g(){ return (yield 1) >> (yield 2); }
+        var it = g();
+        var r1 = it.next();
+        var r2 = it.next(8);
+        var r3 = it.next(2);
+        r1.value === 1 && r1.done === false &&
+        r2.value === 2 && r2.done === false &&
+        r3.done === true && r3.value === 2
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn unsigned_right_shift_with_yield_in_both_operands() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g(){ return (yield 1) >>> (yield 2); }
+        var it = g();
+        var r1 = it.next();
+        var r2 = it.next(-1);
+        var r3 = it.next(0);
+        r1.value === 1 && r1.done === false &&
+        r2.value === 2 && r2.done === false &&
+        r3.done === true && r3.value === 4294967295
       "#,
     )
     .unwrap();
@@ -122,7 +193,9 @@ fn in_operator_with_yield_in_both_operands() {
         var r1 = it.next();
         var r2 = it.next('a');
         var r3 = it.next({a:1});
-        r1.value === 1 && r2.value === 2 && r3.done === true && r3.value === true
+        r1.value === 1 && r1.done === false &&
+        r2.value === 2 && r2.done === false &&
+        r3.done === true && r3.value === true
       "#,
     )
     .unwrap();
@@ -137,10 +210,12 @@ fn instanceof_operator_with_yield_in_both_operands() {
       r#"
         function* g(){ return (yield 1) instanceof (yield 2); }
         var it = g();
-        it.next();
-        it.next({});
-        var r = it.next(Object);
-        r.done === true && r.value === true
+        var r1 = it.next();
+        var r2 = it.next({});
+        var r3 = it.next(Object);
+        r1.value === 1 && r1.done === false &&
+        r2.value === 2 && r2.done === false &&
+        r3.done === true && r3.value === true
       "#,
     )
     .unwrap();
@@ -158,9 +233,10 @@ fn bigint_mixing_error_is_catchable_under_yield() {
           catch (e) { return e && e.name === 'TypeError'; }
         }
         var it = g();
-        it.next();
-        var r = it.next(1);
-        r.done === true && r.value === true
+        var r1 = it.next();
+        var r2 = it.next(1);
+        r1.value === 0 && r1.done === false &&
+        r2.done === true && r2.value === true
       "#,
     )
     .unwrap();
@@ -178,9 +254,10 @@ fn bigint_unsigned_right_shift_throws_type_error_under_yield() {
           catch (e) { return e && e.name === 'TypeError'; }
         }
         var it = g();
-        it.next();
-        var r = it.next(1n);
-        r.done === true && r.value === true
+        var r1 = it.next();
+        var r2 = it.next(1n);
+        r1.value === 0 && r1.done === false &&
+        r2.done === true && r2.value === true
       "#,
     )
     .unwrap();
