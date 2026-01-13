1df8111707b27fcda2f8eb060d3fdaefde28842e
Wilson Lin <code@wilsonl.in>
2026-01-13 13:42:47 -0800

    test(vm-js): cover anonymous class name inference for generator assignment across yield

--- vendor/ecma-rs/vm-js/tests/generators_assignment_anonymous_function_name_inference_across_yield.rs
+++ vendor/ecma-rs/vm-js/tests/generators_assignment_anonymous_function_name_inference_across_yield.rs
@@ -54,3 +54,50 @@ fn generator_property_assignment_sets_anonymous_function_name_across_yield() {
   assert_eq!(v, Value::Bool(true));
 }
 
+#[test]
+fn generator_binding_assignment_sets_anonymous_class_name_across_yield() {
+  let mut rt = new_runtime();
+  let v = rt
+    .exec_script(
+      r#"
+        function* g() {
+          var C;
+          C = (yield 0, class {});
+          return C.name;
+        }
+
+        var it = g();
+        var r1 = it.next();
+        var r2 = it.next();
+
+        r1.value === 0 && r1.done === false &&
+        r2.value === "C" && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(v, Value::Bool(true));
+}
+
+#[test]
+fn generator_property_assignment_sets_anonymous_class_name_across_yield() {
+  let mut rt = new_runtime();
+  let v = rt
+    .exec_script(
+      r#"
+        function* g() {
+          var o = {};
+          o.C = (yield 0, class {});
+          return o.C.name;
+        }
+
+        var it = g();
+        var r1 = it.next();
+        var r2 = it.next();
+
+        r1.value === 0 && r1.done === false &&
+        r2.value === "C" && r2.done === true
+      "#,
+    )
+    .unwrap();
+  assert_eq!(v, Value::Bool(true));
+}
