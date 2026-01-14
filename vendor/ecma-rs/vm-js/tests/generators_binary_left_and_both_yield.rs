742802b574ccf083f9d01c00ea7ec056b755a49e
Wilson Lin <code@wilsonl.in>
2026-01-13 16:17:43 -0800

    test(vm-js): add more generator binary yield cases

--- vendor/ecma-rs/vm-js/tests/generators_binary_left_and_both_yield.rs
+++ vendor/ecma-rs/vm-js/tests/generators_binary_left_and_both_yield.rs
@@ -45,6 +45,86 @@ fn generator_binary_multiplication_yield_on_lhs_only() {
 }
 
 #[test]
+fn generator_binary_subtraction_yield_on_both_sides() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      function* g(){ return (yield 1) - (yield 2); }
+      var it = g();
+      var r1 = it.next();
+      var r2 = it.next(10);
+      var r3 = it.next(3);
+      r1.value === 1 && r1.done === false &&
+      r2.value === 2 && r2.done === false &&
+      r3.done === true && r3.value === 7
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_binary_division_yield_on_both_sides() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      function* g(){ return (yield 1) / (yield 2); }
+      var it = g();
+      var r1 = it.next();
+      var r2 = it.next(20);
+      var r3 = it.next(4);
+      r1.value === 1 && r1.done === false &&
+      r2.value === 2 && r2.done === false &&
+      r3.done === true && r3.value === 5
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_binary_remainder_yield_on_both_sides() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      function* g(){ return (yield 1) % (yield 2); }
+      var it = g();
+      var r1 = it.next();
+      var r2 = it.next(20);
+      var r3 = it.next(6);
+      r1.value === 1 && r1.done === false &&
+      r2.value === 2 && r2.done === false &&
+      r3.done === true && r3.value === 2
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_binary_exponentiation_yield_on_both_sides() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      function* g(){ return (yield 1) ** (yield 2); }
+      var it = g();
+      var r1 = it.next();
+      var r2 = it.next(2);
+      var r3 = it.next(3);
+      r1.value === 1 && r1.done === false &&
+      r2.value === 2 && r2.done === false &&
+      r3.done === true && r3.value === 8
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
 fn generator_binary_strict_equality_yield_on_both_sides() {
   let mut rt = new_runtime();
   let value = rt
@@ -83,4 +163,3 @@ fn generator_binary_relational_comparison_yield_on_both_sides() {
     .unwrap();
   assert_eq!(value, Value::Bool(true));
 }
-
