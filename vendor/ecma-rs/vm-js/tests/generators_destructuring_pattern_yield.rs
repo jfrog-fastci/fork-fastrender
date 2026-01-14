3e50aec5cafcef2e79aa4ac4e28844b21b195abd
Wilson Lin <code@wilsonl.in>
2026-01-14 01:39:44 -0800

    test(vm-js): extend yield-in-pattern generator coverage
    - Add binding-pattern default initializer coverage for var object/array destructuring.
    - Add yield-in-pattern coverage for catch parameters and for-of LHS (both assignment and var decl).

--- vendor/ecma-rs/vm-js/tests/generators_destructuring_pattern_yield.rs
+++ vendor/ecma-rs/vm-js/tests/generators_destructuring_pattern_yield.rs
@@ -94,3 +94,115 @@ fn generator_var_decl_object_destructuring_computed_key_from_yield_resumption() 
   assert_eq!(value, Value::Bool(true));
 }
 
+#[test]
+fn generator_var_decl_object_destructuring_default_from_yield_resumption() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
+          var {a: x = yield 0} = {};
+          return x;
+        }
+        var it = g();
+        var r1 = it.next();
+        var r2 = it.next(7);
+        r1.done === false && r1.value === 0 &&
+        r2.done === true && r2.value === 7
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_var_decl_array_destructuring_default_from_yield_resumption() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
+          var [a = yield 0] = [];
+          return a;
+        }
+        var it = g();
+        var r1 = it.next();
+        var r2 = it.next(9);
+        r1.done === false && r1.value === 0 &&
+        r2.done === true && r2.value === 9
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_catch_param_object_destructuring_computed_key_from_yield_resumption() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
+          try {
+            throw {m: 3};
+          } catch ({[yield 0]: x}) {
+            return x;
+          }
+        }
+        var it = g();
+        var r1 = it.next();
+        var r2 = it.next("m");
+        r1.done === false && r1.value === 0 &&
+        r2.done === true && r2.value === 3
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_for_of_object_destructuring_assignment_computed_key_from_yield_resumption() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
+          var xs = [{m: 1}];
+          var x;
+          for ({[yield 0]: x} of xs) {
+            return x;
+          }
+        }
+        var it = g();
+        var r1 = it.next();
+        var r2 = it.next("m");
+        r1.done === false && r1.value === 0 &&
+        r2.done === true && r2.value === 1
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_for_of_var_decl_object_destructuring_computed_key_from_yield_resumption() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        function* g() {
+          var xs = [{m: 4}];
+          for (var {[yield 0]: x} of xs) {
+            return x;
+          }
+        }
+        var it = g();
+        var r1 = it.next();
+        var r2 = it.next("m");
+        r1.done === false && r1.value === 0 &&
+        r2.done === true && r2.value === 4
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
