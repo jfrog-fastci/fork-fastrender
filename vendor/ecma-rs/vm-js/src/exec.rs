7767a7e679ea0444ff99198b12b513ca5eecc2af
Wilson Lin <code@wilsonl.in>
2026-01-13 22:50:59 -0800

    test(vm-js): add computed super update regression
    Adds a derived-constructor regression for an arrow created before super() that executes super[key()]++.
    
    Asserts:
    - calling before super() throws the expected ReferenceError without evaluating the computed key
    - after super(), key evaluation occurs once and getter/setter run with the derived receiver

--- vendor/ecma-rs/vm-js/src/exec.rs
+++ vendor/ecma-rs/vm-js/src/exec.rs
@@ -50586,6 +50586,46 @@ mod tests {
   }
 
   #[test]
+  fn arrow_this_in_derived_constructor_computed_super_property_update_key_not_evaluated_before_super(
+  ) -> Result<(), VmError> {
+    let source = r#"
+      let log = [];
+      function key() { log.push('key'); return 'x'; }
+
+      class B {
+        constructor() { this._x = 1; }
+        get x() { log.push('get:' + this._x); return this._x; }
+        set x(v) { log.push('set:' + v + ':' + (this instanceof D)); this._x = v; }
+      }
+      class D extends B {
+        constructor() {
+          let f = () => super[key()]++;
+
+          let errName;
+          let errMsg;
+          try { f(); } catch (e) { errName = e.name; errMsg = e.message; }
+
+          super();
+          this.v = f();
+          this.errName = errName;
+          this.errMsg = errMsg;
+        }
+      }
+
+      let d = new D();
+      d.v === 1 &&
+        d._x === 2 &&
+        d.errName === 'ReferenceError' &&
+        d.errMsg === "Must call super constructor in derived class before accessing 'this'" &&
+        log.join(',') === 'key,get:1,set:2:true'
+    "#;
+
+    assert_eq!(eval_script_interpreter(source)?, Value::Bool(true));
+    assert_eq!(eval_script_compiled(source)?, Value::Bool(true));
+    Ok(())
+  }
+
+  #[test]
   fn arrow_this_in_derived_constructor_computed_super_property_call_key_not_evaluated_before_super(
   ) -> Result<(), VmError> {
     let source = r#"
