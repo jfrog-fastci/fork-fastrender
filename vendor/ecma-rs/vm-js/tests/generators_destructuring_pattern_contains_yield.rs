2e62a3634e99e6cc9e308cca285d4af749f6ab06
Wilson Lin <code@wilsonl.in>
2026-01-13 16:51:28 -0800

    test(vm-js): cover yield destructuring evaluation order

--- vendor/ecma-rs/vm-js/tests/generators_destructuring_pattern_contains_yield.rs
+++ vendor/ecma-rs/vm-js/tests/generators_destructuring_pattern_contains_yield.rs
@@ -134,3 +134,92 @@ fn generator_yield_in_array_destructuring_two_defaults_yields_twice() {
     .unwrap();
   assert_eq!(value, Value::Bool(true));
 }
+
+#[test]
+fn generator_yield_in_object_destructuring_computed_key_then_default_evaluation_order() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      (() => {
+        var steps = [];
+        var a = 0;
+
+        var rhs = new Proxy({x: undefined}, {
+          get: function(t, k, _r) {
+            steps.push("get:" + String(k));
+            return t[k];
+          },
+        });
+
+        var res;
+        function* g() {
+          res = ({[(steps.push("key"), (yield 1))]: a = (steps.push("default"), (yield 2))} = rhs);
+          return res === rhs && a === 7;
+        }
+
+        var it = g();
+
+        var r1 = it.next();
+        if (r1.done !== false || r1.value !== 1) return false;
+        if (steps.join("|") !== "key") return false;
+
+        var r2 = it.next("x");
+        if (r2.done !== false || r2.value !== 2) return false;
+        if (steps.join("|") !== "key|get:x|default") return false;
+
+        var r3 = it.next(7);
+        return (
+          r3.done === true &&
+          r3.value === true &&
+          steps.join("|") === "key|get:x|default"
+        );
+      })()
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_yield_in_array_destructuring_default_evaluation_order() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      (() => {
+        var steps = [];
+        var a = 0;
+        var arr = new Proxy([undefined], {
+          get: function(t, k, r) {
+            steps.push(String(k));
+            return Reflect.get(t, k, r);
+          }
+        });
+
+        var res;
+        function* g() {
+          res = ([a = (steps.push("default"), (yield 1))] = arr);
+          return res === arr && a === 3;
+        }
+
+        var it = g();
+
+        var r1 = it.next();
+        if (r1.done !== false || r1.value !== 1) return false;
+
+        var idxLength = steps.indexOf("length");
+        var idx0 = steps.indexOf("0");
+        var idxDefault = steps.indexOf("default");
+        if (idxLength < 0 || idx0 < 0 || idxDefault < 0) return false;
+        if (!(idxLength < idx0 && idx0 < idxDefault)) return false;
+        if (idxDefault !== steps.length - 1) return false;
+
+        var r2 = it.next(3);
+        return r2.done === true && r2.value === true && idxDefault === steps.length - 1;
+      })()
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
