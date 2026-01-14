d13bbb148ced1cd2652b4cc16ddcaf064dd2fac2
Wilson Lin <code@wilsonl.in>
2026-01-14 07:46:52 -0800

    test(vm-js): cover super destructuring with poisoned __proto__
    Add AST + compiled-path regression coverage for destructuring assignment targets (e.g. { x: super[...] } = ...) under a poisoned Object.prototype.__proto__ getter.

--- vendor/ecma-rs/vm-js/tests/super_prop_poisoned_underscore_proto.rs
+++ vendor/ecma-rs/vm-js/tests/super_prop_poisoned_underscore_proto.rs
@@ -51,3 +51,49 @@ fn super_prop_does_not_consult_poisoned_proto_compiled() -> Result<(), VmError> 
   assert_eq!(value, Value::Bool(true));
   Ok(())
 }
+
+#[test]
+fn super_destructuring_assignment_does_not_consult_poisoned_proto() -> Result<(), VmError> {
+  let mut rt = new_runtime();
+  let value = rt.exec_script(
+    r#"
+      Object.defineProperty(Object.prototype, "__proto__", {
+        get() { throw "poison"; }
+      });
+
+      ({ m() {
+        ({ x: super['CONSTRUCTOR'.toLowerCase()] } = { x: 123 });
+        return this.constructor === 123;
+      } }).m()
+    "#,
+  )?;
+
+  assert_eq!(value, Value::Bool(true));
+  Ok(())
+}
+
+#[test]
+fn super_destructuring_assignment_does_not_consult_poisoned_proto_compiled() -> Result<(), VmError> {
+  let mut rt = new_runtime();
+  let script = CompiledScript::compile_script(
+    rt.heap_mut(),
+    "<inline>",
+    r#"
+      Object.defineProperty(Object.prototype, "__proto__", {
+        get() { throw "poison"; }
+      });
+
+      ({ m() {
+        ({ x: super['CONSTRUCTOR'.toLowerCase()] } = { x: 123 });
+        return this.constructor === 123;
+      } }).m()
+    "#,
+  )?;
+  assert!(
+    !script.requires_ast_fallback,
+    "test should execute on the compiled (HIR) path"
+  );
+  let value = rt.exec_compiled_script(script)?;
+  assert_eq!(value, Value::Bool(true));
+  Ok(())
+}
