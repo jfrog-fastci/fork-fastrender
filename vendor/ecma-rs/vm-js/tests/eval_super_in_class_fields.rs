f18e4002d8c85f314ced1bf44f1b09b81a5e2a52
Wilson Lin <code@wilsonl.in>
2026-01-14 06:07:38 -0800

    test(vm-js): cover super in direct eval for class fields
    - Add tests ensuring direct eval in derived class field initializers can access super properties (public/private + static)
    - Add test that indirect eval rejects super with SyntaxError and does not run computed-key side effects

---
+++ vendor/ecma-rs/vm-js/tests/eval_super_in_class_fields.rs
@@ -0,0 +1,80 @@
+use vm_js::{Heap, HeapLimits, JsRuntime, Value, Vm, VmOptions};
+
+fn new_runtime() -> JsRuntime {
+  let vm = Vm::new(VmOptions::default());
+  let heap = Heap::new(HeapLimits::new(1024 * 1024, 1024 * 1024));
+  JsRuntime::new(vm, heap).unwrap()
+}
+
+#[test]
+fn direct_eval_allows_super_in_instance_field_initializer() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+       class B { get x() { return 1; } }
+       class A extends B { y = eval("super.x"); }
+       (new A()).y
+     "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Number(1.0));
+}
+
+#[test]
+fn direct_eval_allows_super_in_private_instance_field_initializer() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      class B { get x() { return 3; } }
+      class A extends B {
+        #y = eval("super.x");
+        get y() { return this.#y; }
+      }
+      (new A()).y
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Number(3.0));
+}
+
+#[test]
+fn direct_eval_allows_super_in_static_field_initializer() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      class B { static get x() { return 2; } }
+      class A extends B { static y = eval("super.x"); }
+      A.y
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Number(2.0));
+}
+
+#[test]
+fn indirect_eval_rejects_super_in_field_initializer_without_side_effects() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+      var side = 0;
+      function sideEffect() { side++; return "k"; }
+      var e = eval;
+      class B { get x() { return 1; } }
+      class A extends B {
+        y = e("({ [sideEffect()]: super.x })");
+      }
+      try {
+        new A();
+        false
+      } catch (err) {
+        err.name === "SyntaxError" && side === 0
+      }
+    "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
