1a3608eb067d85d3756ce5ee233ad998913013e5
Wilson Lin <code@wilsonl.in>
2026-01-14 01:23:50 -0800

    fix: remove merge duplicates blocking browser_ui builds

--- src/api/browser_tab.rs
+++ src/api/browser_tab.rs
@@ -8722,10 +8722,8 @@ mod tests {
   use super::*;
 
   use crate::api::FastRender;
-  use crate::api::VmJsBrowserTabExecutor;
   use crate::js::window_timers::{event_loop_mut_from_hooks, VmJsEventLoopHooks};
   use crate::js::{Clock, VirtualClock, WindowRealm, WindowRealmConfig, WindowRealmHost};
-  use crate::VmJsBrowserTabExecutor;
   use crate::resource::{FetchedResource, ResourceFetcher};
 
   use std::cell::RefCell;
