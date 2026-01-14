c1f5a2b8763df992d4941e566b7d207d8746dc41
Wilson Lin <code@wilsonl.in>
2026-01-13 23:12:32 -0800

    fix(parse-js): restore vendored build
    - Add vendor/ecma-rs/Cargo.lock so nested workspace commands can run with --locked
    - Fix Parser constructors to stop initializing removed disallow_arguments_in_class_init field
    - Add regression test covering arguments restrictions in class static blocks (Ecma error; TS allowed)

---
+++ vendor/ecma-rs/parse-js/tests/class_init_arguments.rs
@@ -0,0 +1,66 @@
+use parse_js::error::SyntaxErrorType;
+use parse_js::lex::Lexer;
+use parse_js::parse::Parser;
+use parse_js::{Dialect, ParseOptions, SourceType};
+
+#[test]
+fn arguments_identifier_reference_is_syntax_error_in_class_static_block_ecma() {
+  let src = r#"
+    class C {
+      static {
+        arguments;
+      }
+    }
+  "#;
+  let opts = ParseOptions {
+    dialect: Dialect::Ecma,
+    source_type: SourceType::Script,
+  };
+  let mut parser = Parser::new(Lexer::new(src), opts);
+  let err = parser
+    .parse_top_level()
+    .expect_err("parse unexpectedly succeeded");
+  assert_eq!(
+    err.typ,
+    SyntaxErrorType::ExpectedSyntax(
+      "`arguments` is not allowed in class field initializers or static initialization blocks"
+    )
+  );
+  assert_eq!(err.actual_token, None);
+}
+
+#[test]
+fn arguments_identifier_reference_is_allowed_in_class_static_block_ts() {
+  let src = r#"
+    class C {
+      static {
+        arguments;
+      }
+    }
+  "#;
+  let opts = ParseOptions {
+    dialect: Dialect::Ts,
+    source_type: SourceType::Module,
+  };
+  let mut parser = Parser::new(Lexer::new(src), opts);
+  let res = parser.parse_top_level();
+  assert!(res.is_ok(), "parse failed: {res:?}");
+}
+
+#[test]
+fn class_static_block_without_arguments_parses() {
+  let src = r#"
+    class C {
+      static {
+        let x = 1;
+      }
+    }
+  "#;
+  let opts = ParseOptions {
+    dialect: Dialect::Ecma,
+    source_type: SourceType::Script,
+  };
+  let mut parser = Parser::new(Lexer::new(src), opts);
+  let res = parser.parse_top_level();
+  assert!(res.is_ok(), "parse failed: {res:?}");
+}
