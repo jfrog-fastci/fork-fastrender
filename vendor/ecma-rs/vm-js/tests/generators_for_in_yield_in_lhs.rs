5dde0a324f12a6d07448a9f45d1f44e429af86cc
Wilson Lin <code@wilsonl.in>
2026-01-14 08:33:32 -0800

    test(vm-js): cover yield in computed-member base in for-in/of loop heads
    - Add for-in/for-of tests where the computed member assignment target base is (yield 1), resuming with the receiver object.
    - Verifies the computed key is evaluated after resumption by mutating the key variable between next() calls.
    - Covers direct assignment targets and destructuring assignment targets (object/array, elem/rest).

--- vendor/ecma-rs/vm-js/tests/generators_for_in_yield_in_lhs.rs
+++ vendor/ecma-rs/vm-js/tests/generators_for_in_yield_in_lhs.rs
@@ -689,3 +689,142 @@ fn generator_for_in_yield_in_array_pattern_rest_assignment_target_computed_membe
     .unwrap();
   assert_eq!(value, Value::Bool(true));
 }
+
+#[test]
+fn generator_for_in_yield_in_assignment_target_computed_member_base_yield_evaluates_key_after_resume() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        (() => {
+          var k = "x";
+          var resumed = {};
+          function* g() {
+            for ((yield 1)[k] in {abc: 0}) { return 0; }
+          }
+          var it = g();
+          var r1 = it.next();
+          k = "k";
+          var r2 = it.next(resumed);
+          return r1.done === false && r1.value === 1 &&
+            r2.done === true && r2.value === 0 &&
+            resumed.k === "abc" &&
+            Object.prototype.hasOwnProperty.call(resumed, "x") === false;
+        })()
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_for_in_yield_in_object_pattern_prop_assignment_target_computed_member_base_yield_evaluates_key_after_resume()
+{
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        (() => {
+          var k = "x";
+          var resumed = {};
+          function* g() {
+            for ({1: (yield 1)[k]} in {abc: 0}) { return 0; }
+          }
+          var it = g();
+          var r1 = it.next();
+          k = "k";
+          var r2 = it.next(resumed);
+          return r1.done === false && r1.value === 1 &&
+            r2.done === true && r2.value === 0 &&
+            resumed.k === "b" &&
+            Object.prototype.hasOwnProperty.call(resumed, "x") === false;
+        })()
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_for_in_yield_in_object_pattern_rest_assignment_target_computed_member_base_yield_evaluates_key_after_resume()
+{
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        (() => {
+          var k = "x";
+          var resumed = {};
+          function* g() {
+            for ({...(yield 1)[k]} in {abc: 0}) { return 0; }
+          }
+          var it = g();
+          var r1 = it.next();
+          k = "k";
+          var r2 = it.next(resumed);
+          return r1.done === false && r1.value === 1 &&
+            r2.done === true && r2.value === 0 &&
+            resumed.k[0] === "a" && resumed.k[1] === "b" && resumed.k[2] === "c" &&
+            Object.prototype.hasOwnProperty.call(resumed, "x") === false;
+        })()
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_for_in_yield_in_array_pattern_elem_assignment_target_computed_member_base_yield_evaluates_key_after_resume()
+{
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        (() => {
+          var k = "x";
+          var resumed = {};
+          function* g() {
+            for ([(yield 1)[k]] in {abc: 0}) { return 0; }
+          }
+          var it = g();
+          var r1 = it.next();
+          k = "k";
+          var r2 = it.next(resumed);
+          return r1.done === false && r1.value === 1 &&
+            r2.done === true && r2.value === 0 &&
+            resumed.k === "a" &&
+            Object.prototype.hasOwnProperty.call(resumed, "x") === false;
+        })()
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_for_in_yield_in_array_pattern_rest_assignment_target_computed_member_base_yield_evaluates_key_after_resume()
+{
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        (() => {
+          var k = "x";
+          var resumed = {};
+          function* g() {
+            for ([...(yield 1)[k]] in {abc: 0}) { return 0; }
+          }
+          var it = g();
+          var r1 = it.next();
+          k = "k";
+          var r2 = it.next(resumed);
+          return r1.done === false && r1.value === 1 &&
+            r2.done === true && r2.value === 0 &&
+            resumed.k.join("") === "abc" &&
+            Object.prototype.hasOwnProperty.call(resumed, "x") === false;
+        })()
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
--- vendor/ecma-rs/vm-js/tests/generators_for_of_yield_in_lhs.rs
+++ vendor/ecma-rs/vm-js/tests/generators_for_of_yield_in_lhs.rs
@@ -980,3 +980,142 @@ fn generator_for_of_const_default_initializer_has_tdz_across_yield() {
     .unwrap();
   assert_eq!(value, Value::Bool(true));
 }
+
+#[test]
+fn generator_for_of_yield_in_assignment_target_computed_member_base_yield_evaluates_key_after_resume() {
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        (() => {
+          var k = "x";
+          var resumed = {};
+          function* g() {
+            for ((yield 1)[k] of [3]) { return 0; }
+          }
+          var it = g();
+          var r1 = it.next();
+          k = "k";
+          var r2 = it.next(resumed);
+          return r1.done === false && r1.value === 1 &&
+            r2.done === true && r2.value === 0 &&
+            resumed.k === 3 &&
+            Object.prototype.hasOwnProperty.call(resumed, "x") === false;
+        })()
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_for_of_yield_in_object_pattern_prop_assignment_target_computed_member_base_yield_evaluates_key_after_resume()
+{
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        (() => {
+          var k = "x";
+          var resumed = {};
+          function* g() {
+            for ({a: (yield 1)[k]} of [{a: 3}]) { return 0; }
+          }
+          var it = g();
+          var r1 = it.next();
+          k = "k";
+          var r2 = it.next(resumed);
+          return r1.done === false && r1.value === 1 &&
+            r2.done === true && r2.value === 0 &&
+            resumed.k === 3 &&
+            Object.prototype.hasOwnProperty.call(resumed, "x") === false;
+        })()
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_for_of_yield_in_object_pattern_rest_assignment_target_computed_member_base_yield_evaluates_key_after_resume()
+{
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        (() => {
+          var k = "x";
+          var resumed = {};
+          function* g() {
+            for ({...(yield 1)[k]} of [{a: 1, b: 2}]) { return 0; }
+          }
+          var it = g();
+          var r1 = it.next();
+          k = "k";
+          var r2 = it.next(resumed);
+          return r1.done === false && r1.value === 1 &&
+            r2.done === true && r2.value === 0 &&
+            resumed.k.b === 2 &&
+            Object.prototype.hasOwnProperty.call(resumed, "x") === false;
+        })()
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_for_of_yield_in_array_pattern_elem_assignment_target_computed_member_base_yield_evaluates_key_after_resume()
+{
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        (() => {
+          var k = "x";
+          var resumed = {};
+          function* g() {
+            for ([(yield 1)[k]] of [[3]]) { return 0; }
+          }
+          var it = g();
+          var r1 = it.next();
+          k = "k";
+          var r2 = it.next(resumed);
+          return r1.done === false && r1.value === 1 &&
+            r2.done === true && r2.value === 0 &&
+            resumed.k === 3 &&
+            Object.prototype.hasOwnProperty.call(resumed, "x") === false;
+        })()
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
+
+#[test]
+fn generator_for_of_yield_in_array_pattern_rest_assignment_target_computed_member_base_yield_evaluates_key_after_resume()
+{
+  let mut rt = new_runtime();
+  let value = rt
+    .exec_script(
+      r#"
+        (() => {
+          var k = "x";
+          var resumed = {};
+          function* g() {
+            for ([...(yield 1)[k]] of [[1, 2, 3]]) { return 0; }
+          }
+          var it = g();
+          var r1 = it.next();
+          k = "k";
+          var r2 = it.next(resumed);
+          return r1.done === false && r1.value === 1 &&
+            r2.done === true && r2.value === 0 &&
+            resumed.k[1] === 2 &&
+            Object.prototype.hasOwnProperty.call(resumed, "x") === false;
+        })()
+      "#,
+    )
+    .unwrap();
+  assert_eq!(value, Value::Bool(true));
+}
